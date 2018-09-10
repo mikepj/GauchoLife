a8661c6768b01974565e717322c3d4d09b6d03fe
Mike Piatek-Jimenez <mike@gauchosoft.com>
2018-09-10 14:35:16 -0400

    Initial Commit

---
+++ Gaucho Life/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Gaucho Life
+//
+//  Created by Mike Piatek-Jimenez on 9/10/18.
+//  Copyright © 2018 Mike Piatek-Jimenez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Gaucho Life/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Gaucho Life
+//
+//  Created by Mike Piatek-Jimenez on 9/10/18.
+//  Copyright © 2018 Mike Piatek-Jimenez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
